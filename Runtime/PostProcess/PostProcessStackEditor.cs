139ee1a33f2c1b5082808c698fef2cca19924731
AggroBird <jan@aggrobird.com>
2023-02-13 14:36:29 +0900

    Removed post process in prefab preview scenes

--- Runtime/PostProcess/PostProcessStackEditor.cs
+++ Runtime/PostProcess/PostProcessStackEditor.cs
@@ -1,35 +1,58 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace AggroBird.GRP
 {
     internal partial class PostProcessStack
     {
+        private static readonly List<GameObject> sceneObjects = new List<GameObject>();
+        private static readonly List<Camera> sceneCameras = new List<Camera>();
+
         private bool TryFindPostProcessCamera(out PostProcessCamera postProcessCamera)
         {
+            if (camera.TryGetComponent(out postProcessCamera) && postProcessCamera.enabled)
+            {
+                return true;
+            }
+
 #if UNITY_EDITOR
             if (camera.cameraType == CameraType.SceneView)
             {
-                postProcessCamera = null;
-
-                if (!SceneView.currentDrawingSceneView.sceneViewState.showImageEffects)
+                Scene currentScene;
+                if (camera.scene.IsValid())
                 {
-                    return false;
+                    currentScene = camera.scene;
                 }
-
-                foreach (Camera camera in Object.FindObjectsOfType<Camera>())
+                else
                 {
-                    if (camera.tag == "MainCamera" && camera.TryGetComponent(out postProcessCamera) && postProcessCamera.enabled)
+                    currentScene = SceneManager.GetActiveScene();
+                    if (!currentScene.IsValid())
                     {
-                        return true;
+                        return false;
                     }
                 }
 
-                return false;
+                currentScene.GetRootGameObjects(sceneObjects);
+                foreach (GameObject sceneObject in sceneObjects)
+                {
+                    sceneObject.GetComponentsInChildren(false, sceneCameras);
+                    foreach (Camera sceneCamera in sceneCameras)
+                    {
+                        if (sceneCamera == camera) continue;
+                        if (!sceneCamera.gameObject.activeInHierarchy) continue;
+                        if (!sceneCamera.enabled) continue;
+                        if (sceneCamera.tag != "MainCamera") continue;
+                        if (sceneCamera.TryGetComponent(out postProcessCamera) && postProcessCamera.enabled)
+                        {
+                            return true;
+                        }
+                    }
+                }
             }
 #endif
 
-            return camera.TryGetComponent(out postProcessCamera) && postProcessCamera.enabled;
+            return false;
         }
     }
 }
