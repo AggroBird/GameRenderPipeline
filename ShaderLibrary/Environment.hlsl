a43d61705b8b4a361d680c57b2e2ec8a2613e83b
Jan de Graaf <jan@aggrobird.com>
2025-10-16 12:02:57 +0900

    Changed fog calculation to be per fragment instead of per vertex

--- ShaderLibrary/Environment.hlsl
+++ ShaderLibrary/Environment.hlsl
@@ -11,56 +11,36 @@ float3 _FogInscatteringColor;
 float3 _FogLightDirection;
 float4 _FogParam;
 
-// x = distance fog factor
-// y = vertical fog factor
-// z = light direction dot
-// w = viewdir y
 #define FOG_ATTRIBUTE_TYPE float4
 
-FOG_ATTRIBUTE_TYPE ComputeFogFactor(VertexPositions vertexPositions)
+float ComputeFogFactor(float fogAttributeCSZ)
 {
-    float clipZ_01 = UNITY_Z_0_FAR_FROM_CLIPSPACE(vertexPositions.positionCS.z);
-
-    FOG_ATTRIBUTE_TYPE result;
-
 #if defined(_FOG_LINEAR)
     // factor = (end-z)/(end-start) = z * (-1/(end-start)) + (end/(end-start))
-    result.x = saturate(clipZ_01 * _FogParam.x + _FogParam.y);
+    return saturate(UNITY_Z_0_FAR_FROM_CLIPSPACE(fogAttributeCSZ) * _FogParam.x + _FogParam.y);
 #elif defined(_FOG_EXP) || defined(_FOG_EXP2)
     // factor = exp(-(density*z)^2)
     // -density * z computed at vertex
-    result.x = _FogParam.x * clipZ_01;
+    return _FogParam.x * UNITY_Z_0_FAR_FROM_CLIPSPACE(fogAttributeCSZ);
 #else
-    result.x = 0;
+    return 0;
 #endif
-
-
-    float3 viewDir = (vertexPositions.positionWS - _WorldSpaceCameraPos);
-    float viewLen = length(viewDir);
-    viewDir /= viewLen;
-
-    result.y = pow(max(dot(viewDir, _FogLightDirection) - saturate(-viewDir.y), 0), 8);
-    result.z = viewLen;
-    result.w = viewDir.y;
-    //result.w = vertexPositions.positionWS.y;
-
-    return result;
 }
 
-float ComputeFogIntensity(FOG_ATTRIBUTE_TYPE fogAttribute)
+float ComputeFogIntensity(float fogAttribute)
 {
     float fogIntensity = 0.0;
 
 #if defined(_FOG_EXP)
     // factor = exp(-density*z)
     // fogFactor = density*z compute at vertex
-    fogIntensity = 1 - saturate(exp2(-fogAttribute.x));
+    fogIntensity = 1 - saturate(exp2(-fogAttribute));
 #elif defined(_FOG_EXP2)
     // factor = exp(-(density*z)^2)
     // fogFactor = density*z compute at vertex
-    fogIntensity = 1 - saturate(exp2(-fogAttribute.x * fogAttribute.x));
+    fogIntensity = 1 - saturate(exp2(-fogAttribute * fogAttribute));
 #elif defined(_FOG_LINEAR)
-    fogIntensity = 1 - fogAttribute.x;
+    fogIntensity = 1 - fogAttribute;
 #endif
 
     return fogIntensity * _FogAmbientColor.a;
@@ -68,17 +48,21 @@ float ComputeFogIntensity(FOG_ATTRIBUTE_TYPE fogAttribute)
 
 void ApplyFog(inout float3 rgb, FOG_ATTRIBUTE_TYPE fogAttribute)
 {
-    float3 fogColor = lerp(_FogAmbientColor.rgb, _FogInscatteringColor, saturate(fogAttribute.y));
-    rgb = lerp(rgb, fogColor, ComputeFogIntensity(fogAttribute));
+    float fogFactor = ComputeFogFactor(fogAttribute.x);
+    float3 viewDir = normalize(fogAttribute.yzw - _WorldSpaceCameraPos);
+    float inscatteringBlend = pow(max(dot(viewDir, _FogLightDirection) - saturate(-viewDir.y), 0), 8);
+    float3 fogColor = lerp(_FogAmbientColor.rgb, _FogInscatteringColor, saturate(inscatteringBlend));
+    rgb = lerp(rgb, fogColor, ComputeFogIntensity(fogFactor));
 }
 
 void BlendFog(inout float3 rgb, FOG_ATTRIBUTE_TYPE fogAttribute)
 {
-    rgb = lerp(rgb, float3(0, 0, 0), ComputeFogIntensity(fogAttribute));
+    float fogFactor = ComputeFogFactor(fogAttribute.x);
+    rgb = lerp(rgb, float3(0, 0, 0), ComputeFogIntensity(fogFactor));
 }
 
 #define FOG_ATTRIBUTE(idx) FOG_ATTRIBUTE_TYPE fogAttribute : TEXCOORD##idx;
-#define TRANSFER_FOG(o, v) o.fogAttribute = ComputeFogFactor(v)
+#define TRANSFER_FOG(o, v) o.fogAttribute = float4(vertexPositions.positionCS.z, vertexPositions.positionWS)
 #define APPLY_FOG(i, rgb) ApplyFog(rgb, i.fogAttribute)
 #define BLEND_FOG(i, rgb) BlendFog(rgb, i.fogAttribute)
 
